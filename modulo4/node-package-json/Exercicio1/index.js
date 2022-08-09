f7c9a1d63bbff0d30dce9dceaa5dafd69e9f3f87
Ari Himmelstein <ahimmelstein@gmail.com>
2022-08-09 19:03:18 -0300

    Exercicio Node e package.json

---
+++ modulo4/node-package-json/Exercicio1/index.js
@@ -0,0 +1,8 @@
+// Exercicio 1
+
+const nome = process.argv[2]
+const idade= process.argv[3]
+const novaidade = Number(idade) + 7
+
+
+console.log("Olá,",nome,"! Você tem",idade,". Em sete anos você terá" , novaidade)
