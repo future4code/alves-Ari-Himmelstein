9b22d21b5d3787b027871c17434b167c6f38292c
Ari Himmelstein <ahimmelstein@gmail.com>
2022-08-31 22:09:51 -0300

    servicos-backend

---
+++ modulo4/servicos-backend/src/data/insertAddress.ts
@@ -0,0 +1,9 @@
+
+import { Address } from "../types"
+import { connection } from "./connection"
+
+export const insertAddress = async (address: Address) => {
+    await connection
+        .insert(address)
+        .into("webservices_exercicio")
+}
